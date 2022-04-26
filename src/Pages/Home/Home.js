efa21c611c45f48d0b44826c46b6cd6cd91aed56
Matthew Forrest <matthew.forrest@fiserv.com>
2022-04-26 12:03:16 +0100

    Added mui styling for table

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,23 +1,21 @@
 import React, { useState, useEffect } from 'react'
+import TextField from '@mui/material/TextField'; 
+import Container from '@mui/material/Container';
 
 export default function Home() {
   const localApiKey = "apiKey";
-  const [apiKey, setApiKey] = useState("");
-
-  useEffect(() => {
+  const [apiKey, setApiKey] = useState(() => {
     const jsonValue = localStorage.getItem(localApiKey);
-    const savedApiKey = JSON.parse(jsonValue);
-    setApiKey(savedApiKey);
-  }, [])
+    return JSON.parse(jsonValue);
+  });
 
   useEffect(() => {
     localStorage.setItem(localApiKey, JSON.stringify(apiKey));
   }, [apiKey]);
 
   return (
-    <div className="content">
-      <div>Home</div>
-      <label>Api Key (saved into local storage)<input type="text" name={localApiKey} value={apiKey} onChange={(e) => setApiKey(e.target.value)}/></label>
-    </div>
+    <Container>
+      <TextField id="apiKey" label="Api Key" variant="outlined" value={apiKey} onChange={(e) => setApiKey(e.target.value)}/>
+    </Container>
   )
 }
